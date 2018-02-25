1752a4e9871f601cf20b1f07951badef03a6e4d1
Steven <steven.james.correia@gmail.com>
2018-02-25 14:15:44 +0100

    Adds correct SQL query (using a view) to find 3 most popular articles

--- loganalysis.py
+++ loganalysis.py
@@ -7,35 +7,43 @@ import psycopg2
 
 DBNAME = "dbname=news"
 
+SQL_THREE_POPULAR_ARTICLES = "select title, num from popular limit 3;"
+
+SQL_POPULAR_AUTHORS = "select name, sum(num) from popular group by name \
+    order by sum desc;"
+
 db = psycopg2.connect(DBNAME)
 
 
-def get_most_popular_articles():
-    """Return the 3 most popular articles from the 'news' database"""
+def get_three_most_popular_articles():
+    """Return the 3 most popular articles from the 'news' database
+    This function uses the view 'popular', please see the README file
+    for instructions on creating this view in the 'news database'."""
     db = psycopg2.connect(DBNAME)
     cur = db.cursor()
-    cur.execute("select articles.title, count(log.path) as num \
-        from articles left join log on log.path like \
-        concat('%',articles.slug,'%') group by articles.title \
-        order by num desc limit 3;")
+    cur.execute(SQL_THREE_POPULAR_ARTICLES)
     list_articles = cur.fetchall()
     return list_articles
     db.close()
 
 
-def get_info_from_logs():
-    """Return interesting info for one entry from the 'log' table"""
+def get_most_popular_authors():
+    """Return interesting info for one entry from the 'log' table
+    This function uses the view 'popular', please see the README file
+    for instructions on creating this view in the 'news database'."""
     db = psycopg2.connect(DBNAME)
     cur = db.cursor()
-    cur.execute("select * from log")
-    list_logs = cur.fetchmany(3)
-    return list_logs
+    cur.execute(SQL_POPULAR_AUTHORS)
+    list_authors = cur.fetchall()
+    return list_authors
     db.close()
 
 
 if __name__ == '__main__':
-    # article_list = get_most_popular_articles()
-    # print(article_list)
-    log_list = get_info_from_logs()
-    for log_output in log_list:
-        print(log_output)
+    article_list = get_three_most_popular_articles()
+    print("The three most popular articles of all time:")
+    for article in article_list:
+        print("\t%s - %d views" % (article[0], article[1]))
+
+    authors_list = get_most_popular_authors()
+    print(authors_list)
